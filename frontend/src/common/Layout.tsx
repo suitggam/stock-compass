b21a7da46f92e6029658eb0a08b4d88d3efbab16
DESKTOP-HA97GMI\SSAFY <jayji5204@naver.com>
2025-09-09 17:00:44 +0900

    feat: 네비게이션 개발

--- frontend/src/common/Layout.tsx
+++ frontend/src/common/Layout.tsx
@@ -1,12 +1,14 @@
 import { Outlet } from "react-router";
+import Header from "./header";
+import Footer from "./Footer";
 
 function Layout() {
   return (
-    <>
-      <header>헤더입니다.</header>
+    <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <Header />
       <Outlet />
-      <footer>푸터입니다.</footer>
-    </>
+      <Footer />
+    </div>
   );
 }
 
