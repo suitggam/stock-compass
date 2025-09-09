e713643c86beba3ba5be9d9a7649e94edfb440bb
김대정 <dvkim96@naver.com>
2025-09-09 10:46:17 +0900

    테일윈드 적용

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,6 @@
-import { createRoot } from "react-dom/client";
-import { RouterProvider } from "react-router-dom";
-import Router from "./router/router";
+import { createRoot } from 'react-dom/client';
+import { RouterProvider } from 'react-router-dom';
+import Router from './router/router';
+import './index.css';
 
-createRoot(document.getElementById("root")!).render(
-  <RouterProvider router={Router} />
-);
+createRoot(document.getElementById('root')!).render(<RouterProvider router={Router} />);
