c9d7d8177db6f8adceae72dd2019634e12394941
DESKTOP-HA97GMI\SSAFY <jayji5204@naver.com>
2025-09-09 17:47:58 +0900

    feat: 푸터 개발

--- frontend/src/common/Header.tsx
+++ frontend/src/common/Header.tsx
@@ -2,7 +2,7 @@ import { useState, type ChangeEvent } from "react";
 import { Link, useNavigate } from "react-router";
 import logoImg from "../assets/logo.webp";
 
-export default function PremiumNavyHeader() {
+export default function Header() {
   const [search, setSearch] = useState<string>("");
   const navigate = useNavigate();
 
--- frontend/src/common/Layout.tsx
+++ frontend/src/common/Layout.tsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router";
-import Header from "./header";
 import Footer from "./Footer";
+import Header from "./Header";
 
 function Layout() {
   return (
