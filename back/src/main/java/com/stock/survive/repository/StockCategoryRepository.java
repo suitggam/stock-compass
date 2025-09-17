0980c562d157bde79642985aa913bd9b5037090d
김대정 <dvkim96@naver.com>
2025-09-17 10:49:23 +0900

    fix: 머지 테스트중

--- back/src/main/java/com/stock/survive/repository/StockCategoryRepository.java
+++ back/src/main/java/com/stock/survive/repository/StockCategoryRepository.java
@@ -1,7 +1,6 @@
 package com.stock.survive.repository;
 
 import com.stock.survive.entity.StockCategory;
-import com.stock.survive.entity.StockItems;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
