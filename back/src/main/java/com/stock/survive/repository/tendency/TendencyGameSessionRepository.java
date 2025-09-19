822df4b6bf859e867b570e742f97528618c329ee
donghyeon <gmj49@naver.com>
2025-09-19 18:05:30 +0900

    feat: 임시 커밋

---
+++ back/src/main/java/com/stock/survive/repository/tendency/TendencyGameSessionRepository.java
@@ -0,0 +1,16 @@
+﻿package com.stock.survive.repository.tendency;
+
+import com.stock.survive.entity.tendency.TendencyGameSession;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface TendencyGameSessionRepository extends JpaRepository<TendencyGameSession, Long> {
+
+    Optional<TendencyGameSession> findByIdAndUser_Id(Long id, Integer userId);
+
+    List<TendencyGameSession> findByUser_IdOrderByStartedAtDesc(Integer userId);
+}
