adf3153fcfaef0c892a8eab88b8789e7af5f1574
김종재 <gghs703@naver.com>
2025-09-19 10:45:11 +0900

    Update file LoginPage.tsx

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { Link } from 'react-router';
 import { useAuth } from '../stores/auth';
 
-const API_BASE = import.meta.env.VITE_API_BASE_URL ?? 'http://localhost:8080';
+const API_BASE = 'http://j13a301.p.ssafy.io:8080/';
 
 export default function LoginPage() {
   const { user, loading, bootstrap, logout } = useAuth();
