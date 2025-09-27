a8eff25d0004391bac9575741f333811eb47da52
DJKIM <dvkim96@naver.com>
2025-09-28 02:37:43 +0900

    fix: 회원탈퇴 기능 수정

--- back/src/main/java/com/stock/survive/repository/GameResultRepository.java
+++ back/src/main/java/com/stock/survive/repository/GameResultRepository.java
@@ -2,12 +2,20 @@ package com.stock.survive.repository;
 
 import com.stock.survive.entity.GameResult;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface GameResultRepository extends JpaRepository<GameResult, Long> {
     
     Optional<GameResult> findTopByUserNoOrderByCreatedAtDesc(Long userNo);
+
+    @Modifying(clearAutomatically = true, flushAutomatically = true)
+    @Query("delete from GameResult gr where gr.userNo = :userId")
+    int deleteByUserId(@Param("userId") Long userId);
 }
